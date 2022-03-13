afcb6c5f7560b67035621925a1a50f77e5993fc2
Anniegavr <gavrilita.anastasia@isa.utm.md>
2022-03-13 11:53:14 +0200

    delete todoscript

--- js/app.js
+++ js/app.js
@@ -1,14 +1,28 @@
 "use strict";
+const buttons = document.querySelectorAll(".btn");
+let i = 0, length = buttons.length;
+// for (i; i < length; i++) {
+//     if (document.addEventListener) {
+//         buttons[i].addEventListener("click", function() {
+//             // use keyword this to target clicked button
+//         });
+//     } else {
+//         buttons[i].attachEvent("onclick", function() {
+//             // use buttons[i] to target clicked button
+//         });
+//     }
+// }
 window.onload=function(){
-    let task = document.querySelector(".BOX2");
+    let task = document.querySelectorAll(".BOX2");
 
-    let list = document.querySelector(".line");
+    let list = document.querySelectorAll(".line");
 
-    let addTask = document.querySelector(".btn");
+    let addTask = document.querySelectorAll(".btn");
+    for (i; i < length; i++) {
+        if (document.addEventListener){
+            addTask.addEventListener("click" , ()=>{
 
-    addTask.addEventListener("click" , ()=>{
-
-        list.insertAdjacentHTML(`afterbegin` , `<li>
+                list.insertAdjacentHTML(`afterbegin` , `<li>
     
                 <i class="fas fa-trash-alt deleteTask"></i>
                 
@@ -16,14 +30,49 @@ window.onload=function(){
                 
             </li>`);
 
-        task.value = "";
+                task.value = "";
 
-        let deleteTask = document.querySelectorAll(".deleteTask");
+                let deleteTask = document.querySelectorAll(".deleteTask");
 
-        deleteTask.forEach(del =>{
+                deleteTask.forEach(del =>{
 
-            del.addEventListener("click" , ()=>{
-                del.parentElement.remove();
-            })
-        })
-    })}
+                    del.addEventListener("click" , ()=>{
+                        del.parentElement.remove();
+                    })
+                })
+            })} else {
+            buttons[i].attachEvent("onclick", function() {
+
+                // use buttons[i] to target clicked button
+            });
+        }
+        }
+    // else {
+    //     buttons[i].attachEvent("onclick", function() {
+    //         // use buttons[i] to target clicked button
+    //     });
+    // }
+    }
+
+    // addTask.addEventListener("click" , ()=>{
+    //
+    //     list.insertAdjacentHTML(`afterbegin` , `<li>
+    //
+    //             <i class="fas fa-trash-alt deleteTask"></i>
+    //
+    //             <h4>${task.value}</h4>
+    //
+    //         </li>`);
+    //
+    //     task.value = "";
+    //
+    //     let deleteTask = document.querySelectorAll(".deleteTask");
+    //
+    //     deleteTask.forEach(del =>{
+    //
+    //         del.addEventListener("click" , ()=>{
+    //             del.parentElement.remove();
+    //         })
+    //     })
+    // })}
+
